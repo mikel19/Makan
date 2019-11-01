07b3be4683fd6b0b2757e6ccf05913d806ea01a0
mikel19 <mikel.agirre@opendeusto.es>
2019-11-01 16:38:44 +0100

    Primera ventana Inicial

--- src/main/java/LP/App.java
+++ src/main/java/LP/App.java
@@ -1,4 +1,4 @@
-package Makan;
+package LP;
 
 /**
  * Hello world!
---
+++ src/main/java/LP/ventanaPrincipal.java
@@ -0,0 +1,50 @@
+package LP;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class ventanaPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ventanaPrincipal frame = new ventanaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ventanaPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 831, 675);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("GESTOR RESTAURANTE MAKAN");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 19));
+		lblNewLabel.setBounds(0, 68, 809, 20);
+		contentPane.add(lblNewLabel);
+	}
+}
